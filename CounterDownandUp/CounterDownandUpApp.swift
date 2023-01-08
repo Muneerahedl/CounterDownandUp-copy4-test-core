8264fca1f116d4e47428ad6a8100a60d815ec6a2
Muneerahedl <116567595+Muneerahedl@users.noreply.github.com>
2023-01-08 17:15:17 +0300

    Initial Commit

---
+++ CounterDownandUp/CounterDownandUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterDownandUpApp.swift
+//  CounterDownandUp
+//
+//  Created by Muneerah Aledaily on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterDownandUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
